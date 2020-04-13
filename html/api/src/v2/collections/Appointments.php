bab2db9bbc01fc7fbf3bf19cc98ac9e8d88f6e58
Joeri <3677joeri@gmail.com>
2020-04-13 23:26:00 +0200

    added "list" functionality for appointments with date range

--- html/api/src/v2/collections/Appointments.php
+++ html/api/src/v2/collections/Appointments.php
@@ -55,8 +55,57 @@ class Appointments {
     }
   }
 
-  private function list() {
+  //modified answer from https://stackoverflow.com/a/12323025
+  private function validateDate($date, $format = 'Y-m-d') {
+    $d = \DateTime::createFromFormat($format, $date);
+    return $d && $d->format($format) == $date;
+  }
 
+  private function list() {
+    //we can only list appointments in a specific timeframe to prevent complete chaos
+    if ($this->validateDate($this->selector)) {
+      //if a second date is set use that date as the end date
+      //will come later
+      $enddate = (isset($this->selector2)) ? $this->selector2 : null;
+      $data = $this->listTimestamp($this->selector, $enddate);
+    }
+    elseif ($this->request->checkSelector()) {
+      $data = $this->listGUID($this->selector);
+    }
+    else {
+      $this->response->sendError(17);
+      return false;
+    }
+    $this->response->sendSuccess($data);
+  }
+
+  private function listTimestamp(string $start = "2000-01-01", string $end = null) {
+    //1 week = 60 * 60 * 24 * 7 = 604800
+    $enddate = strtotime($start) + 604800;
+    $enddate = date("Y-m-d", $enddate);
+    //if an end date is supplied
+    //make sure the range is not more than one month
+    //60 * 60 * 24 * 30 = 2592000
+    if (!is_null($end) && $this->validateDate($end) && (strtotime($end) - strtotime($start) <= 2592000)) {
+      $enddate = $end;
+    }
+
+    $stmt = $this->conn->prepare(
+      "SELECT start, duration, teacher1, teacher2, class, classroom1, classroom2, project, notes, GUID
+      FROM appointments
+      WHERE DATE(start) >= :start AND DATE(start) <= :end
+      "
+    );
+    $stmt->execute([
+      "start" => $start,
+      "end" => $enddate
+    ]);
+
+    return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+  }
+
+  private function listGUID(string $GUID) {
+    return ["working on it"];
   }
 
   private function add() {
