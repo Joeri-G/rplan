e483eb8f18f298b03465b639bf01fc9ce8d15293
Joeri <3677joeri@gmail.com>
2020-04-14 18:20:10 +0200

    Added fallback in case no new login data is supplied

--- settings-setup.sql
+++ settings-setup.sql
@@ -2,8 +2,9 @@ CREATE DATABASE `planner_settings`;
 CREATE TABLE `plannerclients` (
 	`domain` varchar(128) NOT NULL,
 	`db` varchar(128) NOT NULL,
-	`db_user` VARCHAR(128) NOT NULL,
-	`db_password` VARCHAR(128) NOT NULL,
+	`db_user` VARCHAR(128),
+	`db_password` VARCHAR(128),
+	`db_host` VARCHAR(128),
 	`lastChanged` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
 	`active` TINYINT NOT NULL DEFAULT 0,
 	`IP` varchar(64) NOT NULL COMMENT 'ip from where entry was added',
