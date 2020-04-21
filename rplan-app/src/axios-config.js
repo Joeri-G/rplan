d1b8f9c72e08503289969056615f4aad58bacdf8
Joeri <3677joeri@gmail.com>
2020-04-21 13:23:03 +0200

    fixed typo

--- rplan-app/src/axios-config.js
+++ rplan-app/src/axios-config.js
@@ -5,6 +5,6 @@ export default axios.create({
   baseURL: 'http://localhost/rplan/html/api/v2',
   responseType: 'json',
   headers: {
-    Authorization: (typeof localStorage.api_key === 'string') ? ('Bearer: ' + localStorage.api_key) : null
+    Authorization: (typeof localStorage.api_key === 'string') ? ('Bearer ' + localStorage.api_key) : null
   }
 });
