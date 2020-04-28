057d253b4c5d109c54debf549010a1a50ef4a3b2
Joeri <3677joeri@gmail.com>
2020-04-28 17:24:40 +0200

    removed return true; before actual check

--- rplan-app/src/components/Login.js
+++ rplan-app/src/components/Login.js
@@ -4,7 +4,7 @@ import Load from './Loading';
 import Dropdown from './Dropdown';
 import API from '../axios-config';
 
-console.error('NON-FATAL: There are some checks disabled @ src/components/Login.js:66');
+// console.error('NON-FATAL: There are some checks disabled @ src/components/Login.js:66');
 
 export default class Login extends Component {
   componentDidMount() {
@@ -130,12 +130,9 @@ class Schoolselect extends Component {
 }
 
 function validURL(value) {
-  // ONLY FOR TESTING, REMOVE BEFORE DEPLOY
-  return true;
-
   // Copyright (c) 2010-2013 Diego Perini, MIT licensed
   // https://gist.github.com/dperini/729294
   // see also https://mathiasbynens.be/demo/url-regex
   // modified to allow protocol-relative URLs
-  // return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test( value );
+  return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test( value );
 }
