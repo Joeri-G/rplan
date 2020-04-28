7d6f8d68273d3e476196252c7ef231b3384819d1
Joeri <3677joeri@gmail.com>
2020-04-28 12:45:37 +0200

    fixed wrong path

--- rplan-app/src/axios-config.js
+++ rplan-app/src/axios-config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // import axios and export a configured version
 export default axios.create({
-  baseURL: 'http://localhost/rplan/html/api/v2',
+  baseURL: '%PUBLIC_URL%/api/v2',
   responseType: 'json',
   headers: {
     Authorization: (typeof localStorage.api_key === 'string') ? ('Bearer ' + localStorage.api_key) : null
