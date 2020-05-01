7302e0fc0bfb15687dafd2c6f50c46e61d71182b
Joeri <3677joeri@gmail.com>
2020-05-02 00:10:15 +0200

    added env variable

--- rplan-app/src/axios-config.js
+++ rplan-app/src/axios-config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // import axios and export a configured version
 export default axios.create({
-  baseURL: '%PUBLIC_URL%/api/v2',
+  baseURL: `${process.env.PUBLIC_URL}/api/v2`,
   responseType: 'json',
   headers: {
     Authorization: (typeof localStorage.api_key === 'string') ? ('Bearer ' + localStorage.api_key) : null
